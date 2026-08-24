a1f471fbe4409814228cc9d749e51e68b5f24ee0
jaronhofstetter-max <jaronhofstetter@gmail.com>
2026-08-24 07:03:42 +0200

    Add offline service worker for RESERVE v0.4

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='reserve-v04';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
